4f816354d5545dd7da7810f1eadbfb61ef556727
Marian <neengame@abv.bg>
2025-05-30 02:03:04 +0300

    Initial

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+client_script 'client.lua'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/scripts/*.js',
+    'html/images/*',
+    'html/fonts/*',
+    'html/sounds/*',
+}
